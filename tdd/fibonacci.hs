0556c57f9658a2ee6eedb528a761ec1d6d48d7f3
Moritz Rumpf <moritz.rumpf@gmail.com>
2019-10-22 10:36:19 +0200

    Complete fibonacci cata

--- tdd/fibonacci.hs
+++ tdd/fibonacci.hs
@@ -2,18 +2,20 @@ import Test.Hspec
 
 fib 0 = 0
 fib 1 = 1
-fib 2 = 1
+fib n = fib (n-1) + fib (n-2)
 
 
 -- Tests
 main :: IO ()
 main = hspec $ do
-  describe "fib" $ do
-    it "returns 0 at index 0" $ do
+  describe "fib returning elements of the fibonacci sequence" $ do
+    it "returns the 0th element" $ do
         fib 0 `shouldBe` 0
-    it "returns 1 at index 1" $ do
+    it "returns the 1st element" $ do
         fib 1 `shouldBe` 1
-    it "returns 1 at index 2" $ do
+    it "returns the 2nd element" $ do
         fib 2 `shouldBe` 1
-    it "returns 2 at index 2" $ do
+    it "returns the 3rd element" $ do
         fib 3 `shouldBe` 2
+    it "returns the 9th element" $ do
+        fib 9 `shouldBe` 34
