e272367c71b1932a0a46127d950a1d780856b0ce
Moritz Rumpf <moritz.rumpf@gmail.com>
2019-10-23 10:05:13 +0200

    Refactor: remove duplicated test code

--- tdd/romanNumerals.hs
+++ tdd/romanNumerals.hs
@@ -17,32 +17,22 @@ numerals x | x <= 3 = replicate x 'I'
 main :: IO ()
 main = hspec $ do
   describe "roman numeral converter" $ do
-    it "should create roman literal I" $ do
-      numerals 1 `shouldBe` "I"
-    it "should create roman literal III" $ do
-      numerals 3 `shouldBe` "III"
-    it "should create roman literal M" $ do
-      numerals 1000 `shouldBe` "M"
-    it "should create roman literal X" $ do
-      numerals 10 `shouldBe` "X"
-    it "should create roman literal MMVIII" $ do
-      numerals 2008 `shouldBe` "MMVIII"
-    it "should create roman literal MMVII" $ do
-      numerals 2007 `shouldBe` "MMVII"
-    it "should create roman literal MMVI" $ do
-      numerals 2006 `shouldBe` "MMVI"
-    it "should create roman literal MCMXCIX" $ do
-      numerals 1999 `shouldBe` "MCMXCIX"
-    it "should create roman literal MCMXCVIII" $ do
-      numerals 1998 `shouldBe` "MCMXCVIII"
-    it "should create roman literal MCMXCVII" $ do
-      numerals 1997 `shouldBe` "MCMXCVII"
-    it "should create roman literal MCMXCVI" $ do
-      numerals 1996 `shouldBe` "MCMXCVI"
-    it "should create roman literal VIII" $ do
-      numerals 8 `shouldBe` "VIII"
-    it "should create roman literal VII" $ do
-      numerals 7 `shouldBe` "VII"
-    it "should create roman literal VI" $ do
-      numerals 6 `shouldBe` "VI"
-  
+    let test (arabic, roman) = it ("should convert " ++ show arabic) $ numerals arabic `shouldBe` roman
+        testAll = foldl1 (>>) . map test
+        examples = [
+          (1, "I"),
+          (3, "III"),
+          (1000, "M"),
+          (10, "X"),
+          (2008, "MMVIII"),
+          (2007, "MMVII"),
+          (2006, "MMVI"),
+          (1999, "MCMXCIX"),
+          (1998, "MCMXCVIII"),
+          (1997, "MCMXCVII"),
+          (1996, "MCMXCVI"),
+          (8, "VIII"),
+          (7, "VII"),
+          (6, "VI")]
+
+    testAll examples 
