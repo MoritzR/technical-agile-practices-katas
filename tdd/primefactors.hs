3849874d4cedcbec709230ed97dbe9eb84951b2b
Moritz Rumpf <moritz.rumpf@gmail.com>
2019-10-25 09:36:56 +0200

    Add test case
    TPP: constant -> scalar

--- tdd/primefactors.hs
+++ tdd/primefactors.hs
@@ -1,11 +1,13 @@
 import Test.Hspec    
 
-factors 2 = [2]
+factors x = [x]
 
 
 -- Tests
 main :: IO ()
 main = hspec $ do
   describe "splitting a number into prime factors" $ do
-    it "should split two" $ do
+    it "should split 2" $ do
         factors 2 `shouldBe` [2]
+    it "should split 3" $ do
+        factors 3 `shouldBe` [3]
