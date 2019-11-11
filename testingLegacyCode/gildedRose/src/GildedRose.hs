ebd8ac8f528b63efbf12acc5ddf219f749d59ccd
Moritz Rumpf <moritz.rumpf@gmail.com>
2019-11-11 16:15:54 +0100

    Automatic code reformat

--- testingLegacyCode/gildedRose/src/GildedRose.hs
+++ testingLegacyCode/gildedRose/src/GildedRose.hs
@@ -12,48 +12,41 @@ instance Show Item where
 
 updateItemQuality :: Item -> Int
 updateItemQuality (Item name sellIn quality) =
-  if name /= "Aged Brie"
-    && name /= "Backstage passes to a TAFKAL80ETC concert"
-  then
-  if quality > 0 && name /= "Sulfuras, Hand of Ragnaros"
-  then quality - 1
-  else quality
-  else
-  if quality < 50
-  then
-    quality + 1 +
-      (if name == "Backstage passes to a TAFKAL80ETC concert" && sellIn < 11 && quality < 49
-        then
-          1 + (if sellIn < 6 && quality < 48
-              then 1
-              else 0)
-        else 0)
-  else quality
+  if name /= "Aged Brie" && name /= "Backstage passes to a TAFKAL80ETC concert"
+    then if quality > 0 && name /= "Sulfuras, Hand of Ragnaros"
+      then quality - 1
+      else quality
+    else if quality < 50
+      then
+        quality
+        + 1
+        + (if name
+               == "Backstage passes to a TAFKAL80ETC concert"
+               && sellIn
+               <  11
+               && quality
+               <  49
+            then 1 + (if sellIn < 6 && quality < 48 then 1 else 0)
+            else 0
+          )
+      else quality
 
 updateQuality :: GildedRose -> GildedRose
 updateQuality = map updateQualityItem
-  where
-    updateQualityItem (Item name sellIn quality) =
-      let
-        quality' = updateItemQuality (Item name sellIn quality)
+ where
+  updateQualityItem (Item name sellIn quality) =
+    let quality' = updateItemQuality (Item name sellIn quality)
 
         sellIn' =
-          if name /= "Sulfuras, Hand of Ragnaros"
-          then sellIn - 1
-          else sellIn
-      in
-        if sellIn' < 0
-        then
-          if name /= "Aged Brie"
-          then
-            if name /= "Backstage passes to a TAFKAL80ETC concert"
-            then
-              if quality' > 0 && name /= "Sulfuras, Hand of Ragnaros"
-              then (Item name sellIn' (quality' - 1))
+            if name /= "Sulfuras, Hand of Ragnaros" then sellIn - 1 else sellIn
+    in  if sellIn' < 0
+          then if name /= "Aged Brie"
+            then if name /= "Backstage passes to a TAFKAL80ETC concert"
+              then if quality' > 0 && name /= "Sulfuras, Hand of Ragnaros"
+                then (Item name sellIn' (quality' - 1))
+                else (Item name sellIn' quality')
+              else (Item name sellIn' 0)
+            else if quality' < 50
+              then (Item name sellIn' (quality' + 1))
               else (Item name sellIn' quality')
-            else (Item name sellIn' 0)
-          else
-            if quality' < 50
-            then (Item name sellIn' (quality' + 1))
-            else (Item name sellIn' quality')
-        else (Item name sellIn' quality')
+          else (Item name sellIn' quality')
