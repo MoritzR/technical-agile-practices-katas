b939a7be7554d25789492a972378a2ab841732f2
Moritz Rumpf <moritz.rumpf@gmail.com>
2019-11-11 18:13:24 +0100

    Move aged brie quality calculation
    to the updateQuality function

--- testingLegacyCode/gildedRose/src/GildedRose.hs
+++ testingLegacyCode/gildedRose/src/GildedRose.hs
@@ -11,7 +11,9 @@ instance Show Item where
 
 updateItemQuality :: Item -> Int
 
-updateItemQuality (Item "Aged Brie" sellIn quality) = min (quality + 1) 50
+updateItemQuality (Item "Aged Brie" sellIn quality)
+  | sellIn < 0 = min (quality + 2) 50
+  | otherwise  = min (quality + 1) 50
 
 updateItemQuality (Item "Sulfuras, Hand of Ragnaros" sellIn quality) = quality
 
@@ -35,9 +37,7 @@ updateQualityItem (Item "Aged Brie" sellIn quality) =
   let name     = "Aged Brie"
       quality' = updateItemQuality (Item name sellIn quality)
       sellIn'  = sellIn - 1
-  in  if quality' < 50 && sellIn' < 0
-        then (Item name sellIn' (quality' + 1))
-        else (Item name sellIn' quality')
+  in  Item name sellIn' quality'
 
 updateQualityItem (Item "Backstage passes to a TAFKAL80ETC concert" sellIn quality)
   = let name     = "Backstage passes to a TAFKAL80ETC concert"
