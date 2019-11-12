a9b148fdeae96c18c848a936766f3f112da00e06
Moritz Rumpf <moritz.rumpf@gmail.com>
2019-11-12 10:53:45 +0100

    Extract method

--- testingLegacyCode/gildedRose/src/GildedRose.hs
+++ testingLegacyCode/gildedRose/src/GildedRose.hs
@@ -33,17 +33,18 @@ updateBackStagePassQuality sellIn quality
 updateQuality :: GildedRose -> GildedRose
 updateQuality = map updateItem
 
+sellInAfterOneDay sellIn = sellIn -1
 
 updateItem (Item "Aged Brie" sellIn quality) =
   let name     = "Aged Brie"
       quality' = updateItemQuality (Item name sellIn quality)
-      sellIn'  = sellIn - 1
+      sellIn'  = sellInAfterOneDay sellIn
   in  Item name sellIn' quality'
 
 updateItem (Item "Backstage passes to a TAFKAL80ETC concert" sellIn quality)
   = let name     = "Backstage passes to a TAFKAL80ETC concert"
         quality' = updateItemQuality (Item name sellIn quality)
-        sellIn'  = sellIn - 1
+        sellIn'  = sellInAfterOneDay sellIn
     in  Item name sellIn' quality'
 
 updateItem (Item "Sulfuras, Hand of Ragnaros" sellIn quality) =
@@ -55,5 +56,5 @@ updateItem (Item "Sulfuras, Hand of Ragnaros" sellIn quality) =
 
 updateItem (Item name sellIn quality) =
   let quality' = updateItemQuality (Item name sellIn quality)
-      sellIn'  = sellIn - 1
+      sellIn'  = sellInAfterOneDay sellIn
   in  Item name sellIn' quality'
