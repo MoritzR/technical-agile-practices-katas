1ca7a15aca897d93c6dbf22f8bbce46652235542
Moritz Rumpf <moritz.rumpf@gmail.com>
2019-12-03 16:21:25 +0100

    Add parsing for all directions

--- fourElementsOfSimpleDesign/katacombs/src/Lib.hs
+++ fourElementsOfSimpleDesign/katacombs/src/Lib.hs
@@ -4,7 +4,11 @@ game :: IO ()
 game = putStrLn "Hello World"
 
 parse :: String -> Command
-parse _ = Go North
+parse "go n" = Go North
+parse "go s" = Go South
+parse "go w" = Go West
+parse "go e" = Go East
 
 data Command = Go Direction deriving (Show, Eq)
-data Direction = North deriving (Show, Eq)
+data Direction = North | South | West | East
+    deriving (Show, Eq)
--- fourElementsOfSimpleDesign/katacombs/test/LibSpec.hs
+++ fourElementsOfSimpleDesign/katacombs/test/LibSpec.hs
@@ -8,3 +8,9 @@ spec =
   describe "parse go directions" $ do
     it "should parse 'go n'" $
         Lib.parse "go n" `shouldBe` Go North
+    it "should parse 'go s'" $
+        Lib.parse "go s" `shouldBe` Go South
+    it "should parse 'go w'" $
+        Lib.parse "go w" `shouldBe` Go West
+    it "should parse 'go e'" $
+        Lib.parse "go e" `shouldBe` Go East
