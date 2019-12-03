e745042f34d85eba64c16c9fc8a206a42b90d6e6
Moritz Rumpf <moritz.rumpf@gmail.com>
2019-12-03 16:42:24 +0100

    Add Look command for one direction

--- fourElementsOfSimpleDesign/katacombs/src/Lib.hs
+++ fourElementsOfSimpleDesign/katacombs/src/Lib.hs
@@ -1,5 +1,7 @@
 module Lib where
 
+import Data.List (isPrefixOf)
+
 game :: IO ()
 game = putStrLn "Hello World"
 
@@ -8,8 +10,12 @@ parse "go n" = Just $ Go North
 parse "go s" = Just $ Go South
 parse "go w" = Just $ Go West
 parse "go e" = Just $ Go East
-parse _ = Nothing
+parse s
+    | "look" `isPrefixOf` s  = Just $ Look North
+    | otherwise = Nothing
 
-data Command = Go Direction deriving (Show, Eq)
+data Command = Go Direction
+    | Look Direction
+    deriving (Show, Eq)
 data Direction = North | South | West | East
     deriving (Show, Eq)
--- fourElementsOfSimpleDesign/katacombs/test/LibSpec.hs
+++ fourElementsOfSimpleDesign/katacombs/test/LibSpec.hs
@@ -6,15 +6,19 @@ import Data.Maybe (fromJust)
 
 spec :: Spec
 spec = do
-  describe "parse go directions" $ do
+  describe "parsing commands" $ do
     let parseJust = fromJust . parse
-    it "should parse 'go n'" $
-      parseJust "go n" `shouldBe` Go North
-    it "should parse 'go s'" $
-      parseJust "go s" `shouldBe` Go South
-    it "should parse 'go w'" $
-      parseJust "go w" `shouldBe` Go West
-    it "should parse 'go e'" $
-      parseJust "go e" `shouldBe` Go East
-  it "should return Nothing for an unkown command" $
-    parse "I want to be a butterfly" `shouldBe` Nothing
+    describe "parse go directions" $ do
+      it "should parse 'go n'" $
+        parseJust "go n" `shouldBe` Go North
+      it "should parse 'go s'" $
+        parseJust "go s" `shouldBe` Go South
+      it "should parse 'go w'" $
+        parseJust "go w" `shouldBe` Go West
+      it "should parse 'go e'" $
+        parseJust "go e" `shouldBe` Go East
+    describe "parse look directions" $ do
+      it "should parse 'look n'" $
+        parseJust "look" `shouldBe` Look North
+    it "should return Nothing for an unkown command" $
+      parse "I want to be a butterfly" `shouldBe` Nothing
