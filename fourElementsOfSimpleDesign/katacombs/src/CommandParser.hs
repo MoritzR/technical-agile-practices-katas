2be42f0b61b76a38e017c6d5f148067c604c8b7c
Moritz Rumpf <moritz.rumpf@gmail.com>
2019-12-12 16:56:14 +0100

    Remove "command" postfix from command parsers
    Adding "Command" to the end of every method names seems like noise. That it is a command is already reflected in the type.

--- fourElementsOfSimpleDesign/katacombs/src/CommandParser.hs
+++ fourElementsOfSimpleDesign/katacombs/src/CommandParser.hs
@@ -3,6 +3,7 @@ module CommandParser (parse) where
 import Data.Maybe (listToMaybe)
 import Text.ParserCombinators.ReadP (ReadP, choice, readP_to_S, string)
 import qualified Text.ParserCombinators.ReadP as Parser
+import Prelude hiding (take, drop)
 import Model
 
 parse :: String -> Maybe Command
@@ -11,54 +12,54 @@ parse = listToMaybe . map fst . readP_to_S command
 command :: ReadP Command
 command = do
     c <- choice 
-        [ goCommand
-        , lookCommand
-        , lookAtCommand
-        , takeCommand
-        , bagCommand
-        , dropCommand
-        , lookAroundCommand
+        [ go
+        , look
+        , lookAt
+        , lookAround
+        , take
+        , drop
+        , bag
         ]
     Parser.eof
     return c
 
-goCommand :: ReadP Command
-goCommand = do
+go :: ReadP Command
+go = do
     string "go "
     d <- direction
     return $ Go d
 
-lookCommand :: ReadP Command
-lookCommand = do
+look :: ReadP Command
+look = do
     string "look "
     d <- direction
     return $ Look d
 
-lookAtCommand :: ReadP Command
-lookAtCommand = do
+lookAt :: ReadP Command
+lookAt = do
     string "look "
     item <- Parser.many1 Parser.get
     return $ LookAt (ItemName item)
 
-lookAroundCommand :: ReadP Command
-lookAroundCommand = do
+lookAround :: ReadP Command
+lookAround = do
     string "look"
     return LookAround
 
-takeCommand :: ReadP Command
-takeCommand = do
+take :: ReadP Command
+take = do
     string "take "
     item <- Parser.many1 Parser.get
     return $ Take (ItemName item)
 
-dropCommand :: ReadP Command
-dropCommand = do
+drop :: ReadP Command
+drop = do
     string "drop "
     item <- Parser.many1 Parser.get
     return $ Drop (ItemName item)
 
-bagCommand :: ReadP Command
-bagCommand = do
+bag :: ReadP Command
+bag = do
     string "bag"
     return Bag
 
